92a0facfb1edff30665276b49f45fd7ed23c2f55
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2014-11-20 20:37:21 -0500

    Simplified some of examples so they have fewer dependencies on jboss and JTA as a whole.

--- src/test/java/com/syncleus/grail/graph/hibernate/DogBreedTest.java
+++ src/test/java/com/syncleus/grail/graph/hibernate/DogBreedTest.java
@@ -9,69 +9,45 @@ import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.transaction.TransactionManager;
-import java.lang.reflect.InvocationTargetException;
 
 public class DogBreedTest {
 
-	private static final String JBOSS_TM_CLASS_NAME = "com.arjuna.ats.jta.TransactionManager";
 	private static final Log logger = LoggerFactory.make();
 
 	@Test
 	public void testMain() {
-
-		TransactionManager tm = getTransactionManager();
-
 		//build the EntityManagerFactory as you would build in in Hibernate Core
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory( "ogm-jpa-tutorial" );
 
 		//Persist entities the way you are used to in plain JPA
-		try {
-			tm.begin();
-			EntityManager em = emf.createEntityManager();
-			Breed collie = new Breed();
-			collie.setName( "Collie" );
-			em.persist( collie );
-			Dog dina = new Dog();
-			dina.setName( "Dina" );
-			dina.setBreed( collie );
-			em.persist( dina );
-			Long dinaId = dina.getId();
-			em.flush();
-			em.close();
-			tm.commit();
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tm = em.getTransaction();
+		tm.begin();
+		Breed collie = new Breed();
+		collie.setName( "Collie" );
+		em.persist( collie );
+		Dog dina = new Dog();
+		dina.setName( "Dina" );
+		dina.setBreed( collie );
+		em.persist( dina );
+		Long dinaId = dina.getId();
+		em.flush();
+		em.close();
+		tm.commit();
 
-			//Retrieve your entities the way you are used to in plain JPA
-			tm.begin();
-			em = emf.createEntityManager();
-			dina = em.find( Dog.class, dinaId );
-			Assert.assertEquals("Dina", dina.getName());
-			Assert.assertEquals("Collie", dina.getBreed().getName());
-			em.flush();
-			em.close();
-			tm.commit();
+		//Retrieve your entities the way you are used to in plain JPA
+		em = emf.createEntityManager();
+		tm = em.getTransaction();
+		tm.begin();
+		dina = em.find( Dog.class, dinaId );
+		Assert.assertEquals("Dina", dina.getName());
+		Assert.assertEquals("Collie", dina.getBreed().getName());
+		em.flush();
+		em.close();
+		tm.commit();
 
-			emf.close();
-		} catch ( Exception e ) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public static TransactionManager getTransactionManager() {
-		try {
-			Class<?> tmClass = DogBreedTest.class.getClassLoader().loadClass( JBOSS_TM_CLASS_NAME );
-			return (TransactionManager) tmClass.getMethod( "transactionManager" ).invoke( null );
-		} catch ( ClassNotFoundException e ) {
-			e.printStackTrace();
-		} catch ( InvocationTargetException e ) {
-			e.printStackTrace();
-		} catch ( NoSuchMethodException e ) {
-			e.printStackTrace();
-		} catch ( IllegalAccessException e ) {
-			e.printStackTrace();
-		}
-		return null;
+		emf.close();
 	}
 }
